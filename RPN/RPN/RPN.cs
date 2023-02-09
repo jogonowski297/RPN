a077216484dcb707b8898e703b84795c38170e8f
Jakub Ogonowski <j.ogonowski.519@studms.ug.edu.pl>
2023-02-09 23:43:42 +0100

    Gotowy kod

---
+++ RPN/RPN/RPN.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPNCalulator
+{
+    public class RPN
+    {
+        private Stack<int> _operators;
+        Dictionary<string, Func<int, int, int>> _operationFunction;
+        Dictionary<string, Func<int, int>> _operationFunction2;
+
+        public int EvalRPN(string input)
+        {
+            _operationFunction = new Dictionary<string, Func<int, int, int>>
+            {
+                ["+"] = (fst, snd) => (fst + snd),
+                ["-"] = (fst, snd) => (fst - snd),
+                ["*"] = (fst, snd) => (fst * snd),
+                ["/"] = (fst, snd) => (fst / snd)
+
+            };
+            _operationFunction2 = new Dictionary<string, Func<int, int>>
+            {
+                ["^2"] = fst => ((int)Math.Pow(fst, 2)),
+                ["!"] = fst => silnia(fst),
+                ["||"] = fst => wartBezw(fst),
+                ["??"] = fst => fst
+            };
+
+            _operators = new Stack<int>();
+            
+            if(input == "")
+                throw new Exception("Brak danych");
+
+            var splitInput = input.Split(' ');
+
+            foreach (var op in splitInput)
+            {
+                if (IsNumber(op))
+                    _operators.Push(Int32.Parse(op));
+               
+                if (IsOperator(op))
+                {
+                    var num1 = _operators.Pop();
+                    var num2 = _operators.Pop();
+
+                    if(num1 == 0)
+                    {
+                        throw new Exception("Nie dziel przez zero");
+                    }
+                    _operators.Push(_operationFunction[op](num2, num1));
+                    //_operators.Push(Operation(op)(num1, num2));
+                }
+
+                if (IsUnaryOperation(op))
+                {
+                    var num1 = _operators.Pop();
+                    _operators.Push(_operationFunction2[op](num1));
+
+                }
+
+                if (op[0].Equals('B'))
+                    _operators.Push((int)Convert.ToInt32(op.Substring(1), 2));
+                
+                if (op[0].Equals('D'))
+                    _operators.Push((int)Convert.ToInt32(op.Substring(1), 10));
+                
+                if (op[0].Equals('#'))
+                    _operators.Push((int)Convert.ToInt32(op.Substring(1), 16));
+                
+            }
+
+            var result = _operators.Pop();
+            if (_operators.IsEmpty)
+            {
+                return result;
+            }
+            throw new InvalidOperationException();
+        }
+
+        private int wartBezw(int input)
+        {
+            if (input < 0)
+                return input * (-1);
+            else
+                return input;
+            
+        }
+
+        private int silnia(int input)
+        {
+            var silnia_ = Convert.ToInt32(input);
+            int result = 1;
+            for (int i = 1; i <= silnia_; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        private bool IsNumber(String input) => Int32.TryParse(input, out _);
+
+        private bool IsOperator(String input) =>
+            input.Equals("+") || input.Equals("-") ||
+            input.Equals("*") || input.Equals("/");
+
+        private bool IsUnaryOperation(String input) =>
+            input.Equals("!") || input.Equals("^2") || input.Equals("||")
+            || input.Equals("??");
+
+        private bool IsBinar(char input) =>
+            input.Equals('B') || input.Equals('D') || input.Equals('#');
+
+        private Func<int, int, int> Operation(String input) =>
+            (x, y) =>
+            (
+                (input.Equals("+") ? x + y :
+                    (input.Equals("*") ? x * y : int.MinValue)
+                )
+            );
+    }
+}
---
+++ RPNTest/RPNTest.cs
@@ -0,0 +1,168 @@
+﻿using NUnit.Framework;
+using System;
+using RPNCalulator;
+
+namespace RPNTest {
+	[TestFixture]
+	public class RPNTest {
+		private RPN _sut;
+		[SetUp]
+		public void Setup() {
+			_sut = new RPN();
+		}
+		[Test]
+		public void CheckIfTestWorks() {
+			Assert.Pass();
+		}
+
+		[Test]
+		public void CheckIfCanCreateSut() {
+			Assert.That(_sut, Is.Not.Null);
+		}
+
+		[Test]
+		public void SingleDigitOneInputOneReturn() {
+			var result = _sut.EvalRPN("1");
+
+			Assert.That(result, Is.EqualTo(1));
+
+		}
+		[Test]
+		public void SingleDigitOtherThenOneInputNumberReturn() {
+			var result = _sut.EvalRPN("2");
+
+			Assert.That(result, Is.EqualTo(2));
+
+		}
+		[Test]
+		public void TwoDigitsNumberInputNumberReturn() {
+			var result = _sut.EvalRPN("12");
+
+			Assert.That(result, Is.EqualTo(12));
+
+		}
+		[Test]
+		public void TwoNumbersGivenWithoutOperator_ThrowsExcepton() {
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("1 2"));
+
+		}
+		[Test]
+		public void OperatorPlus_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("1 2 +");
+
+			Assert.That(result, Is.EqualTo(3));
+		}
+		[Test]
+		public void OperatorTimes_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("2 2 *");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+		[Test]
+		public void OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("1 2 -");
+
+			Assert.That(result, Is.EqualTo(-1));
+		}
+		[Test]
+		public void ComplexExpression() {
+			var result = _sut.EvalRPN("15 7 1 1 + - / 3 * 2 1 1 + + -");
+
+			Assert.That(result, Is.EqualTo(5));
+		}
+
+        [Test]
+        public void DzieleniePrzezZero()
+        {
+            string exception = "Nie dziel przez zero";
+
+            var ex = Assert.Throws<Exception>(() => _sut.EvalRPN("15 0 /"));
+
+            Assert.AreEqual(exception, ex.Message);
+        }
+
+        [Test]
+        public void PowFunc()
+        {
+            var result = _sut.EvalRPN("2 ^2");
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Silnia()
+        {
+            var result = _sut.EvalRPN("5 !");
+
+            Assert.That(result, Is.EqualTo(120));
+        }
+
+        [Test]
+        public void WartBezw()
+        {
+            var result = _sut.EvalRPN("1 2 - ||");
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void IsBinarTest()
+        {
+            var result = _sut.EvalRPN("B101 B101 +");
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void IsDecimalTest()
+        {
+            var result = _sut.EvalRPN("D10 D10 +");
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void IsHexTest()
+        {
+            var result = _sut.EvalRPN("#AB #AB +");
+
+            Assert.That(result, Is.EqualTo(342));
+        }
+
+        [Test]
+        public void IsHexAndBinTest()
+        {
+            var result = _sut.EvalRPN("#AB B101 +");
+
+            Assert.That(result, Is.EqualTo(176));
+        }
+
+        [Test]
+        public void IsMinusDecAndBinTest()
+        {
+            var result = _sut.EvalRPN("B101 D10 -");
+
+            Assert.That(result, Is.EqualTo(-5));
+        }
+
+        [Test]
+        public void IsMinusDecAndBinBezwzTest()
+        {
+            var result = _sut.EvalRPN("B101 D10 - ||");
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void PustyStos()
+        {
+            string exception = "Brak danych";
+
+            var ex = Assert.Throws<Exception>(() => _sut.EvalRPN(""));
+
+            Assert.AreEqual(exception, ex.Message);
+        }
+
+
+    }
+}
